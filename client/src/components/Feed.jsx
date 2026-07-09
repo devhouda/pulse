73d0d7be2858252bc7d80cf319695523a0526c4c
devhouda <houdadjemai.1@gmail.com>
2026-07-09 10:15:59 +0100

    style using built into Vite CSS Modules so styles stay scoped per-component

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,17 +1,25 @@
+import styles from "./Feed.module.css";
+
 const Feed = () => {
   return (
-    <div>
-      <div>
-        <h3>Post1</h3>
-        <p>lorem desc pro max iphone pc laptop macbook</p>
+    <div className={styles.feed}>
+      <div className={styles.post}>
+        <h3 className={styles.title}>Post1</h3>
+        <p className={styles.description}>
+          lorem desc pro max iphone pc laptop macbook
+        </p>
       </div>
-      <div>
-        <h3>Post2</h3>
-        <p>lorem desc pro max iphone pc laptop macbook</p>
+      <div className={styles.post}>
+        <h3 className={styles.postTitle}>Post2</h3>
+        <p className={styles.description}>
+          lorem desc pro max iphone pc laptop macbook
+        </p>
       </div>
-      <div>
-        <h3>Post3</h3>
-        <p>lorem desc pro max iphone pc laptop macbook</p>
+      <div className={styles.post}>
+        <h3 className={styles.postTitle}>Post3</h3>
+        <p className={styles.description}>
+          lorem desc pro max iphone pc laptop macbook
+        </p>
       </div>
     </div>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
+import styles from "./Navbar.module.css";
 import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/">Feed</Link>
+    <nav className={styles.navbar}>
+      <Link className={styles.link} to="/">
+        Feed
+      </Link>
       {/* change later to "/profile/:username" */}
-      <Link to="/profile/devhouda">Profile</Link>
-      <Link to="/login">Login</Link>
+      <Link className={styles.link} to="/profile/devhouda">
+        Profile
+      </Link>
+      <Link className={styles.link} to="/login">
+        Login
+      </Link>
     </nav>
   );
 };
